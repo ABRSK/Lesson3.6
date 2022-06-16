4c0f80236b12996cdcf39c42382f25f857749d57
AreyBRSK <areybrsk@gmail.com>
2022-06-16 11:39:19 +0300

    Initial Commit

---
+++ Lesson3.6/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lesson3.6
+//
+//  Created by Андрей Барсук on 16.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lesson3.6/Lesson3_6App.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson3_6App.swift
+//  Lesson3.6
+//
+//  Created by Андрей Барсук on 16.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct Lesson3_6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
